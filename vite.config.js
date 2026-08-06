814f9189a07aae7a8c9fa00269c1e9af2a03fc1f
shopvirsa <lalaop020@gmail.com>
2026-08-07 00:19:00 +0500

    Update API configuration and Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://shopiversa-production.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
